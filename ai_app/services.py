ed867228840250becc2ab4b160743212e8bb2e71
Diyorbek <diyoreshmurodov99@gmail.com>
2026-09-20 19:26:19 +0500

    updateAI(2)

--- ai_app/services.py
+++ ai_app/services.py
@@ -14,7 +14,7 @@ def ai_handler(prompt_text):
 
         # AI modelini yaratish va tizim ko'rsatmasini (system instruction) berish
         model = genai.GenerativeModel(
-            model_name="gemini-2.5-flash",
+            model_name="gemini-3.6-flash",
             system_instruction="Siz professional diyetolog va shaxsiy fitnes trenergiz. Javoblaringizni o'zbek tilida, tushunarli va chiroyli formatda bering."
         )
 
